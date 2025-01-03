9919d81fa032e0e378d93f9a8e7176c62e2ff711
vigneshbalaji07 <Vignesh@satetechnologies.com>
2025-01-03 15:38:53 +0530

    code changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: {
+  //   ppr: "incremental",
+  // },
 };
 
 export default nextConfig;
+
+//CanaryOnlyError: The experimental feature "experimental.ppr" can only be enabled when using the latest canary version of Next.js.
